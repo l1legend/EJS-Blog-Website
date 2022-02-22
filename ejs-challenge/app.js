12b377c2d35e23c0219517e87124aadf052da1ad
Michael Min <michaelmin2020@gmail.com>
2022-02-22 17:11:23 -0600

    challenge 11 and 12 complete

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -15,9 +15,11 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
 
 app.get("/", function(req, res){
   res.render("home", {startingContent: homeStartingContent});
+  console.log(posts);
 });
 
 app.get("/about", function(req, res){
@@ -33,7 +35,13 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-  console.log(req.body.postTitle);
+  console.log(req.body.postBody);
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect("/");
 });
 
 app.listen(3000, function() {
